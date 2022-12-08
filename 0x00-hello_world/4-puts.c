c326c84918f8a19d12ead9d28c826668f6da3a93
TEDYBEY <tewodrosbey@gmail.com>
2022-12-08 11:47:17 -0800

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main -Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"programing is like building a multilingual puzzle");
+	return(0);
+
+}
