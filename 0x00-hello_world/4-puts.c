fb284cff3180f98e87801ff5780e853f4e5a8c2b
TEDYBEY <tewodrosbey@gmail.com>
2022-12-11 05:21:56 -0800

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print a line of code using puts
- *
- * Return: 0
-*/
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-
+	return (0);
 }
