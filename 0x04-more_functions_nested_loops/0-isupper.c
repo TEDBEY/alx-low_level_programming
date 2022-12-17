af9175feb5d9e3ad96d636b4e9702078c02948d2
TEDYBEY <tewodrosbey@gmail.com>
2022-12-16 23:59:43 -0800

    alx

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,10 @@
 
 /**
  * _isupper - checks uppercase
- * @c: parameter
- * Return: 0 if sucessful
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c >= 'A' && c <= 'Z');
 }
